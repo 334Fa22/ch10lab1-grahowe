673a0f9f1cd7baf72058c26c1118d5ea1953929b
grahowe <omgraham02@gmail.com>
2022-09-20 12:17:28 -0500

    Finalized lab

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -8,7 +8,7 @@
 function hamburger(){
   var menu = document.getElementById("menu-links");
   if (menu.style.display === "block") {
-    menu.style.display = "none");
+    menu.style.display = "none";
   } else {
     menu.style.display = "block";
   }
